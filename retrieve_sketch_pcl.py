c71945943c39316347310a403cfa4eb551aef8d0
frankielp <96617665+frankielp@users.noreply.github.com>
2023-03-04 11:59:19 +0700

    raw retrieve

--- retrieve_sketch_pcl.py
+++ retrieve_sketch_pcl.py
@@ -1,4 +1,7 @@
 from common.test import test_loop
+from pointcloud.curvenet import CurveNet
+from pointcloud.dataset import SHREC23_PointCloudData_ImageQuery
+from pointcloud.pointmlp import PointMLP, PointMLPElite
 from ringnet.dataset import SHREC23_Rings_RenderOnly_ImageQuery
 from ringnet.models import Base3DObjectRingsExtractor
 import torch
@@ -14,14 +17,15 @@ import argparse
 parser = argparse.ArgumentParser()
 # Directory 
 parser.add_argument('--info-json', type=str, required=True, help='Path to model infomation json')
-parser.add_argument('--rings-path', type=str, required=True, help='Path to parent folder of ringviews')
+parser.add_argument('--obj-data-path', type=str, required=True, help='Path to 3D objects folder')
 parser.add_argument('--skt-data-path', type=str, required=True, help='Path to 3D sketches folder')
-parser.add_argument('--test-csv-path', type=str, required=True, help='Path to CSV file of sketch in test set')
+parser.add_argument('--test-csv-path', type=str, required=True, help='Path to CSV file of sketch id')
 parser.add_argument('--output-path', type=str,
                     default='./predict', help='Path to output folder')
 
 #Tuning weight
-parser.add_argument('--used-rings', type=str, default='0,1,2,3,4,5,6', help='Rings to be used for training')
+parser.add_argument('--latent-dim', type=int, default=128,
+                    help='Latent dimensions of common embedding space')
 
 parser.add_argument('--obj-weight', type=str, required=True, help='Path to object weight')
 parser.add_argument('--skt-weight', type=str, required=True, help='Path to sketch weight')
@@ -37,19 +41,26 @@ with open(args.info_json) as json_file:
 # Initiate
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 batch_size = arg_dict['batch_size']
-latent_dim = arg_dict['latent_dim']
+latent_dim = args.latent_dim
 obj_weight=args.obj_weight
 skt_weight=args.skt_weight
 output_path=args.output_path
 num_workers=arg_dict['num_workers']
-#ring id (need to review this arg)
-ring_ids = [int(id) for id in args.used_rings.split(',')]
+pcl_model=arg_dict['pcl_model']
 
 
 # Load Model
 ## Object extraction
-obj_kwargs, obj_state = torch.load(args.obj_weight)
-obj_extractor = Base3DObjectRingsExtractor(**obj_kwargs)
+obj_state = torch.load(args.obj_weight)
+if pcl_model == 'curvenet':
+    obj_extractor = CurveNet(device=device)
+elif pcl_model == 'pointmlp':
+    obj_extractor = PointMLP(device=device)
+elif pcl_model == 'pointmlpelite':
+    obj_extractor = PointMLPElite(device=device)
+else:
+    raise NotImplementedError
+
 obj_embedder = MLP(obj_extractor, latent_dim=latent_dim)
 obj_embedder=obj_embedder.load_state_dict(obj_state).to(device)
 
@@ -66,20 +77,21 @@ query_embedder = MLP(query_extractor, latent_dim=latent_dim)
 query_embedder=query_embedder.load_state_dict(query_state).to(device)
 
 # Load data (need run test)
-test_ds = SHREC23_Rings_RenderOnly_ImageQuery(
-        args.test_csv_path, args.rings_path, args.skt_data_path, ring_ids)
+test_ds = SHREC23_PointCloudData_ImageQuery(obj_data_path=args.obj_data_path,
+                                            csv_data_path=args.test_csv_path,
+                                            skt_root=args.skt_data_path)
 
-test_dl = DataLoader(test_ds, batch_size=batch_size, shuffle=False,
-                     num_workers=num_workers, collate_fn=test_ds.collate_fn)
+test_dl = DataLoader(test_ds, batch_size=batch_size,
+                    shuffle=False, num_workers=args.num_workers, collate_fn=test_ds.collate_fn)
 
 
 
 # Inference (need empty target id check)
 test_loop(obj_embedder=obj_embedder, query_embedder=query_embedder,
-                                obj_input='object_ims', query_input='query_ims',
-                                dl=test_dl,
-                                dimension=latent_dim,
-                                device=device,
-                                output_path=output_path)
+            obj_input='pointclouds', query_input='query_ims',
+            dl=test_dl,
+            dimension=latent_dim,
+            output_path = output_path,
+            device=device)
 
 # json to csv or rewrite inference base on test_loop
