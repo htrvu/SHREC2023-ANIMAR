81ddbd09dc85de050822efcd9df8e10eee31e86d
htrvu <trongvulqd@gmail.com>
2023-03-06 00:24:31 +0700

    refactor

--- retrieve_sketch_pcl.py
+++ retrieve_sketch_pcl.py
@@ -2,63 +2,44 @@ import torch
 from torch.utils.data import DataLoader
 from pytorch_metric_learning.losses import NTXentLoss, CrossBatchMemory
 
-from pointcloud.dataset import SHREC23_PointCloudData_ImageQuery
+from pointcloud.dataset import SHREC23_Test_PointCloudData_Objects
 from pointcloud.curvenet import CurveNet
 from pointcloud.pointmlp import PointMLP, PointMLPElite
 
+from common.dataset import SHREC23_Test_SketchesData
 from common.models import ResNetExtractor, EfficientNetExtractor, MLP
-from common.test import test_loop
-from common.train import train_loop
-
-from utils.plot_logs import plot_logs
+from common.predict import predict
 
 import os
 import json
 import argparse
 
-
-'''
-python retrieve_sketch_pcl.py \
-    --pcl-model pointmlp \
-    --cnn-backbone efficientnet_v2_s \
-    --output-path exps \
-    --obj-data-path data/SketchANIMAR2023/3D_Model_References/References \
-    --skt-data-path data/SketchANIMAR2023/Train/CroppedSketchQuery_Train \
-    --test-csv-path data/csv/test_skt.csv \
-    --batch-size 4 \
-    --latent-dim 128 \
-    --obj-weight exps/pointmlp_effiv2_200epoch/weights/best_obj_embedder.pth \
-    --skt-weight exps/pointmlp_effiv2_200epoch/weights/best_query_embedder.pth
-'''
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--pcl-model', type=str,
-                    default='curvenet', choices=['curvenet', 'pointmlp', 'pointmlpelite'], help='Model for point cloud feature extraction')
-parser.add_argument('--cnn-backbone', type=str, default='efficientnet_b2',
-                    choices=[*ResNetExtractor.arch.keys(), *EfficientNetExtractor.arch.keys()],  help='Model for sketch feature extraction')
-parser.add_argument('--output-path', type=str, default='./exps_test', help='Path to output folder')
-
+parser.add_argument('--info-json', type=str, required=True, help='Path to model infomation json')
+parser.add_argument('--output-path', type=str, default='./predicts', help='Path to output folder')
 parser.add_argument('--obj-data-path', type=str, required=True, help='Path to 3D objects folder')
+parser.add_argument('--obj-csv-path', type=str, required=True, help='Path to CSV file of objects')
 parser.add_argument('--skt-data-path', type=str, required=True, help='Path to 3D sketches folder')
-parser.add_argument('--test-csv-path', type=str, required=True, help='Path to CSV file of mapping object and sketch in test set')
-
-parser.add_argument('--batch-size', type=int, default=2, help='Batch size')
-parser.add_argument('--num-workers', type=int, default=1, help='Num of workers')
-
-parser.add_argument('--latent-dim', type=int, default=128, help='Latent dimensions of common embedding space')
-
+parser.add_argument('--skt-csv-path', type=str, help='Path to CSV file of sketches')
 parser.add_argument('--obj-weight', type=str, required=True, help='Path to 3D object weight')
 parser.add_argument('--skt-weight', type=str, required=True, help='Path to sketch weight')
 
 args = parser.parse_args()
 
+
+#Info json
+with open(args.info_json) as json_file:
+    arg_dict = json.load(json_file)
+
+batch_size = arg_dict['batch_size']
+latent_dim = arg_dict['latent_dim']
+
 # Initialize
 ## Model parameters
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-latent_dim = args.latent_dim
 obj_weight=args.obj_weight
 skt_weight=args.skt_weight
-batch_size = args.batch_size
+
 ## Storage
 # output folder
 output_path = args.output_path
@@ -70,29 +51,30 @@ if len(folders) > 0:
     for folder in folders:
         if folder.startswith('ringview'):
             continue
-        new_id = max(new_id, int(folder.split('pcl_exp_')[-1]))
+        new_id = max(new_id, int(folder.split('pcl_predict_')[-1]))
     new_id += 1
-output_path = os.path.join(output_path, f'pcl_exp_{new_id}')
+output_path = os.path.join(output_path, f'pcl_predict_{new_id}')
 os.makedirs(output_path)
 
 # Load Model
 ## Get weight
 ### For Object Extraction
-obj_state, *other = torch.load(args.obj_weight)
+obj_state = torch.load(args.obj_weight)
 ### For Sketch Extraction
 query_kwargs, query_state, *other = torch.load(args.skt_weight)
 
 ## Construct model
 ### For Object Extraction
-if args.pcl_model == 'curvenet':
+if arg_dict['pcl_model'] == 'curvenet':
     obj_extractor = CurveNet(device=device)
-elif args.pcl_model == 'pointmlp':
+elif arg_dict['pcl_model'] == 'pointmlp':
     obj_extractor = PointMLP(device=device)
-elif args.pcl_model == 'pointmlpelite':
+elif arg_dict['pcl_model'] == 'pointmlpelite':
     obj_extractor = PointMLPElite(device=device)
 else:
     raise NotImplementedError
-### For Sketch Extraction
+
+## For Sketch Extraction
 cnn_backbone = str(query_kwargs['version'])
 if cnn_backbone.startswith('resnet'):
     query_extractor = ResNetExtractor(cnn_backbone)
@@ -111,19 +93,24 @@ query_embedder = MLP(query_extractor, latent_dim=latent_dim)
 query_embedder.load_state_dict(query_state)
 query_embedder = query_embedder.to(device)
 
-
 # Load Data
 ## Load test data
-test_ds = SHREC23_PointCloudData_ImageQuery(obj_data_path=args.obj_data_path,
-                                            csv_data_path=args.test_csv_path,
-                                            skt_root=args.skt_data_path)
-## Initialize dataloader for test
-test_dl = DataLoader(test_ds, batch_size=batch_size,
-                    shuffle=False, num_workers=args.num_workers, collate_fn=test_ds.collate_fn)
+obj_ds = SHREC23_Test_PointCloudData_Objects(obj_data_path=args.obj_data_path,
+                                             csv_data_path=args.obj_csv_path)
 
-metrics_results = test_loop(obj_embedder=obj_embedder, query_embedder=query_embedder,
+skt_ds = SHREC23_Test_SketchesData(skt_data_path=args.skt_data_path,
+                                    csv_data_path=args.skt_csv_path)
+
+## Initialize dataloader
+obj_dl = DataLoader(obj_ds, batch_size=batch_size,
+                    shuffle=False, num_workers=arg_dict['num_workers'], collate_fn=obj_ds.collate_fn)
+skt_dl = DataLoader(skt_ds, batch_size=batch_size,
+                    shuffle=False, num_workers=arg_dict['num_workers'], collate_fn=skt_ds.collate_fn)
+
+predict(obj_embedder=obj_embedder, query_embedder=query_embedder,
         obj_input='pointclouds', query_input='query_ims',
-        dl=test_dl,
+        obj_dl=obj_dl,
+        query_dl=skt_dl,
         dimension=latent_dim,
-        output_path = output_path,
+        output_path=output_path,
         device=device)
--- retrieve_sketch_ringview.py
+++ retrieve_sketch_ringview.py
@@ -1,34 +1,33 @@
-from common.test import test_loop
-from ringnet.dataset import SHREC23_Rings_RenderOnly_ImageQuery
-from ringnet.models import Base3DObjectRingsExtractor
 import torch
 from torch.utils.data import DataLoader
 
+from ringnet.dataset import SHREC23_Test_Rings_Objects
+from ringnet.models import Base3DObjectRingsExtractor
+
+from common.predict import predict
 from common.models import MLP, EfficientNetExtractor, ResNetExtractor
+from common.dataset import SHREC23_Test_SketchesData
+
 import numpy as np
 import os
 import json
 import argparse
 
-
 parser = argparse.ArgumentParser()
+
 # Directory 
 parser.add_argument('--info-json', type=str, required=True, help='Path to model infomation json')
 parser.add_argument('--rings-path', type=str, required=True, help='Path to parent folder of ringviews')
+parser.add_argument('--obj-csv-path', type=str, required=True, help='Path to CSV file of objects')
 parser.add_argument('--skt-data-path', type=str, required=True, help='Path to 3D sketches folder')
-parser.add_argument('--test-csv-path', type=str, required=True, help='Path to CSV file of sketch in test set')
+parser.add_argument('--skt-csv-path', type=str, required=True, help='Path to CSV file of sketch in test set')
 parser.add_argument('--obj-weight', type=str, required=True, help='Path to object weight')
 parser.add_argument('--skt-weight', type=str, required=True, help='Path to sketch weight')
 parser.add_argument('--output-path', type=str,
-                    default='./predict', help='Path to output folder')
-
-#Tuning weight
-parser.add_argument('--used-rings', type=str, default='0,1,2,3,4,5,6', help='Rings to be used for training')
-
-
-
+                    default='./predicts', help='Path to output folder')
 
 args = parser.parse_args()
+
 #Info json
 with open(args.info_json) as json_file:
     arg_dict = json.load(json_file)
@@ -43,9 +42,9 @@ if len(folders) > 0:
     for folder in folders:
         if folder.startswith('pcl'):
             continue
-        new_id = max(new_id, int(folder.split('ringview_exp_')[-1]))
+        new_id = max(new_id, int(folder.split('ringview_predict_')[-1]))
     new_id += 1
-sub_output_path = os.path.join(output_path, f'ringview_exp_{new_id}')
+sub_output_path = os.path.join(output_path, f'ringview_predict_{new_id}')
 os.makedirs(sub_output_path)
 
 # Initiate
@@ -56,16 +55,15 @@ obj_weight=args.obj_weight
 skt_weight=args.skt_weight
 output_path=args.output_path
 num_workers=arg_dict['num_workers']
-#ring id (need to review this arg)
-ring_ids = [int(id) for id in args.used_rings.split(',')]
-
+ring_ids = [int(id) for id in arg_dict['used_rings'].split(',')]
 
 # Load Model
 ## Object extraction
 obj_kwargs, obj_state = torch.load(args.obj_weight)
 obj_extractor = Base3DObjectRingsExtractor(**obj_kwargs)
 obj_embedder = MLP(obj_extractor, latent_dim=latent_dim)
-obj_embedder=obj_embedder.load_state_dict(obj_state).to(device)
+obj_embedder.load_state_dict(obj_state)
+obj_embedder = obj_embedder.to(device)
 
 ## sketch extraction
 query_kwargs, query_state = torch.load(args.skt_weight)
@@ -77,23 +75,26 @@ elif skt_cnn_backbone.startswith('efficientnet'):
 else:
     raise NotImplementedError
 query_embedder = MLP(query_extractor, latent_dim=latent_dim)
-query_embedder=query_embedder.load_state_dict(query_state).to(device)
+query_embedder.load_state_dict(query_state)
+query_embedder = query_embedder.to(device)
 
 # Load data (need run test)
-test_ds = SHREC23_Rings_RenderOnly_ImageQuery(
-        args.test_csv_path, args.rings_path, args.skt_data_path, ring_ids)
+obj_ds = SHREC23_Test_Rings_Objects(args.obj_csv_path, args.rings_path, ring_ids)
 
-test_dl = DataLoader(test_ds, batch_size=batch_size, shuffle=False,
-                     num_workers=num_workers, collate_fn=test_ds.collate_fn)
+skt_ds = SHREC23_Test_SketchesData(skt_data_path=args.skt_data_path,
+                                                csv_data_path=args.skt_csv_path)
+
+obj_dl = DataLoader(obj_ds, batch_size=batch_size,
+                    shuffle=False, num_workers=arg_dict['num_workers'], collate_fn=obj_ds.collate_fn)
+skt_dl = DataLoader(skt_ds, batch_size=batch_size,
+                    shuffle=False, num_workers=arg_dict['num_workers'], collate_fn=skt_ds.collate_fn)
 
 
-
-# Inference (need empty target id check)
-test_loop(obj_embedder=obj_embedder, query_embedder=query_embedder,
-                                obj_input='object_ims', query_input='query_ims',
-                                dl=test_dl,
-                                dimension=latent_dim,
-                                device=device,
-                                output_path=output_path)
-
-# json to csv or rewrite inference base on test_loop
+# Inference
+metrics_results = predict(obj_embedder=obj_embedder, query_embedder=query_embedder,
+        obj_input='object_ims', query_input='query_ims',
+        obj_dl=obj_dl,
+        query_dl=skt_dl,
+        dimension=latent_dim,
+        output_path=sub_output_path,
+        device=device)
